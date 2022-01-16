424f6dca789ed5cf04aecd41024ad96169470106
Quasar_Catfish <ctsn99@gmail.com>
2022-01-15 21:49:18 -0500

    Add a way to push slash commands to Discord's global commands

---
+++ src/main/java/com/quas/mythsmagic/UpdateGuildCommands.java
@@ -0,0 +1,35 @@
+package com.quas.mythsmagic;
+
+import java.io.FileReader;
+
+import com.google.gson.Gson;
+import com.quas.mythsmagic.commands.CommandManager;
+import com.quas.mythsmagic.util.BotProperties;
+import com.quas.mythsmagic.util.Constants;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class UpdateGuildCommands {
+	
+	public static void main(String[] args) {
+		
+		try (FileReader in = new FileReader(Constants.FILE_PROPERTIES)) {
+			
+			// Read in properties
+			BotProperties prop = new Gson().fromJson(in, BotProperties.class);
+			
+			// Create bot
+			JDA bot = JDABuilder.createDefault(prop.getToken()).build();
+			bot.awaitReady();
+			
+			// Load commands
+			CommandManager.load(bot, true);
+		} catch (Exception e) {
+			e.printStackTrace();
+			BotProperties.createDefault();
+		}
+		
+		System.exit(0);
+	}
+}
