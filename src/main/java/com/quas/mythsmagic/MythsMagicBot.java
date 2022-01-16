eddfa681b32d5c2afdc641959bc5692101965102
Quasar_Catfish <ctsn99@gmail.com>
2022-01-15 21:48:29 -0500

    Make properties a static variable

--- src/main/java/com/quas/mythsmagic/MythsMagicBot.java
+++ src/main/java/com/quas/mythsmagic/MythsMagicBot.java
@@ -3,6 +3,7 @@ package com.quas.mythsmagic;
 import java.io.FileReader;
 
 import com.google.gson.Gson;
+import com.quas.mythsmagic.commands.CommandManager;
 import com.quas.mythsmagic.util.BotProperties;
 import com.quas.mythsmagic.util.Constants;
 
@@ -11,18 +12,24 @@ import net.dv8tion.jda.api.JDABuilder;
 
 public class MythsMagicBot {
 	
+	private static BotProperties properties;
+	public static BotProperties getProperties() {
+		return properties;
+	}
+	
 	public static void main(String[] args) {
 		
 		try (FileReader in = new FileReader(Constants.FILE_PROPERTIES)) {
 			
 			// Read in properties
-			BotProperties prop = new Gson().fromJson(in, BotProperties.class);
+			properties = new Gson().fromJson(in, BotProperties.class);
 			
 			// Create bot
-			JDA bot = JDABuilder.createDefault(prop.getToken()).build();
+			JDA bot = JDABuilder.createDefault(properties.getToken()).build();
 			bot.awaitReady();
 			
 			// Load commands
+			CommandManager.load(bot, false);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
