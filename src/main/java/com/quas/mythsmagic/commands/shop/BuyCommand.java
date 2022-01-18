a6f277b31d9cbc6c3b0c62a118ae859540e7d4ac
Quasar_Catfish <ctsn99@gmail.com>
2022-01-17 21:46:22 -0500

    Add /buy

---
+++ src/main/java/com/quas/mythsmagic/commands/shop/BuyCommand.java
@@ -0,0 +1,78 @@
+package com.quas.mythsmagic.commands.shop;
+
+import com.quas.mythsmagic.commands.Command;
+import com.quas.mythsmagic.commands.CommandInfo;
+import com.quas.mythsmagic.database.DB;
+import com.quas.mythsmagic.database.Player;
+import com.quas.mythsmagic.database.ShopItem;
+import com.quas.mythsmagic.util.Util;
+
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+
+@CommandInfo(name = "buy", desc = "Buys the specified pack, deck, or bundles, if able")
+public class BuyCommand extends Command {
+	
+	private static final String ITEM = "item";
+	private static final String QUANTITY = "quantity";
+
+	@Override
+	public CommandData data() {
+		CommandData cd = super.data();
+		cd.addOption(OptionType.STRING, ITEM, "The pack, deck, or bundle you wish to buy", true);
+		cd.addOption(OptionType.INTEGER, QUANTITY, "The number you want to buy (default 1)");
+		return cd;
+	}
+	
+	@Override
+	public void handle(SlashCommandEvent event) {
+		Player player = Player.of(event.getUser());
+		
+		long quantity = event.getOption(QUANTITY) == null ? 1 : event.getOption(QUANTITY).getAsLong();
+		if (quantity <= 0) {
+			event.replyFormat("%s, you cannot purchase %,d of an item.", event.getUser().getAsMention(), quantity).setEphemeral(isEphemeral(event)).queue();
+			return;
+		}
+		
+		// Find item whose name most closely matches
+		String itemName = event.getOption(ITEM).getAsString();
+		ShopItem[] items = ShopItem.getActive();
+		ShopItem best = items[0];
+		int bestScore = Util.levenshtein(itemName, best.getShopName());
+		
+		for (ShopItem item : items) {
+			int score = Util.levenshtein(itemName, item.getShopName());
+			if (score < bestScore) {
+				best = item;
+				bestScore = score;
+			}
+		}
+		
+		// Able to buy with starter bundle tickets
+		if (best.getPrice().isBuyableWithStarterBundleTickets() && player.getStarterBundleTickets() >= best.getPrice().getStarterBundleTicketsPrice() * quantity) {
+			DB.update("update `players` set `starterBundleTickets` = `starterBundleTickets` - ? where `playerId` = ?;", best.getPrice().getStarterBundleTicketsPrice() * quantity, player.getPlayerId());
+		}
+		
+		// Able to buy with jewels
+		else if (best.getPrice().isBuyableWithJewels() && player.getMoney() >= best.getPrice().getJewelsPrice() * quantity) {
+			DB.update("update `players` set `money` = `money` - ? where `playerId` = ?;", best.getPrice().getJewelsPrice() * quantity, player.getPlayerId());
+		}
+		
+		// Not able to buy
+		else {
+			event.replyFormat("%s, you cannot afford to buy the %s.", event.getUser().getAsMention(), best.getShopName()).setEphemeral(isEphemeral(event)).queue();
+			return;
+		}
+		
+		// Buy the item
+		if (best.hasStarterDeck()) DB.update("insert into `playerstarterdecks` values(?, ?, ?) on duplicate key update `quantity` = `quantity` + ?;", player.getPlayerId(), best.getStarterDeckId(), quantity, quantity);
+		if (best.hasPacks()) {
+			for (int q = 0; q < best.getPackIds().length; q++) {
+				DB.update("insert into `playerpacks` values(?, ?, ?) on duplicate key update `quantity` = `quantity` + ?;", player.getPlayerId(), best.getPackIds()[q], best.getPackQuantities()[q] * quantity, best.getPackQuantities()[q] * quantity);
+			}
+		}
+		
+		event.replyFormat("%s, you bought the %s for %s.", event.getUser().getAsMention(), best.getShopName(), best.getPrice()).setEphemeral(isEphemeral(event)).queue();
+	}
+}
--- src/main/java/com/quas/mythsmagic/util/Util.java
+++ src/main/java/com/quas/mythsmagic/util/Util.java
@@ -1,8 +1,43 @@
 package com.quas.mythsmagic.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Util {
 
 	public static String quantity(long quantity, String object, String pluralize) {
 		return String.format("%,d %s%s", quantity, object, quantity == 1 ? "" : pluralize);
+	}
+	
+	public static int levenshtein(String stringA, String stringB) {
+		char[] a = process(stringA);
+		char[] b = process(stringB);
+		int[][] memo = new int[a.length + 1][b.length + 1];
+		
+		for (int q = 0; q <= a.length; q++) memo[q][0] = q;
+		for (int w = 0; w <= b.length; w++) memo[0][w] = w;
+		
+		for (int q = 1; q <= a.length; q++) {
+			for (int w = 1; w <= b.length; w++) {
+				int cost = a[q - 1] == b[w - 1] ? 0 : 1;
+				memo[q][w] = min(memo[q - 1][w - 1] + cost, memo[q - 1][w] + 1, memo[q][w - 1] + 1);
+				
+				if (q > 1 && w > 1 && a[q - 1] == b[w - 2] && a[q - 2] == b[w - 1]) {
+					memo[q][w] = min(memo[q][w], memo[q - 2][w - 2] + cost);
+				}
+			}
+		}
+		
+		return memo[a.length][b.length];
+	}
+	
+	private static char[] process(String s) {
+		return StringUtils.stripAccents(s).toLowerCase().replaceAll("\\W", "").toCharArray();
+	}
+	
+	public static int min(int...arr) {
+		if (arr.length == 0) return 0;
+		int min = arr[0];
+		for (int x : arr) if (x < min) min = x;
+		return min;
 	}
 }
